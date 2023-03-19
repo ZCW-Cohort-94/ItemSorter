e2aa4c1b340175e52d0eb8e42736e1628cee00d8
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-19 16:41:34 -0400

    Finished PriceComparator and tested

--- src/test/java/rocks/zipcode/io/comparators/pricecomparatortest/NegativeCompareToTest.java
+++ src/test/java/rocks/zipcode/io/comparators/pricecomparatortest/NegativeCompareToTest.java
@@ -15,8 +15,8 @@ public class NegativeCompareToTest {
     public void test1() {
         // given
         Comparator<Item> priceComparator = (Comparator<Item>)new PriceComparator();
-        Item item1 = new Item(null, null, 0.0);
-        Item item2 = new Item(null, null, 1.0);
+        Item item1 = new Item(1L, "Apple", 0.0);
+        Item item2 = new Item(2L, "Steak", 1.0);
 
         // when
         Integer comparisonValue = priceComparator.compare(item1, item2);
@@ -29,8 +29,8 @@ public class NegativeCompareToTest {
     public void test2() {
         // given
         Comparator<Item> priceComparator = (Comparator<Item>)new PriceComparator();
-        Item item1 = new Item(null, null, 1.0);
-        Item item2 = new Item(null, null, 1.1);
+        Item item1 = new Item(5L, "k", 1.0);
+        Item item2 = new Item(6L, "l", 1.1);
 
         // when
         Integer comparisonValue = priceComparator.compare(item1, item2);
@@ -44,8 +44,8 @@ public class NegativeCompareToTest {
     public void test3() {
         // given
         Comparator<Item> priceComparator = (Comparator<Item>)new PriceComparator();
-        Item item1 = new Item(null, null, 2.5);
-        Item item2 = new Item(null, null, 7.0);
+        Item item1 = new Item(1L, "a", 2.5);
+        Item item2 = new Item(2L, "b", 7.0);
 
         // when
         Integer comparisonValue = priceComparator.compare(item1, item2);
@@ -58,8 +58,8 @@ public class NegativeCompareToTest {
     public void test4() {
         // given
         Comparator<Item> priceComparator = (Comparator<Item>)new PriceComparator();
-        Item item1 = new Item(null, null, 10.0);
-        Item item2 = new Item(null, null, 20.0);
+        Item item1 = new Item(2L, "a", 10.0);
+        Item item2 = new Item(3L, "f", 20.0);
 
         // when
         Integer comparisonValue = priceComparator.compare(item1, item2);
@@ -72,8 +72,8 @@ public class NegativeCompareToTest {
     public void test5() {
         // given
         Comparator<Item> priceComparator = (Comparator<Item>)new PriceComparator();
-        Item item1 = new Item(null, null, 15.0);
-        Item item2 = new Item(null, null, 17.0);
+        Item item1 = new Item(1L, "A", 15.0);
+        Item item2 = new Item(2L, "B", 17.0);
 
         // when
         Integer comparisonValue = priceComparator.compare(item1, item2);
@@ -87,8 +87,8 @@ public class NegativeCompareToTest {
     public void test6() {
         // given
         Comparator<Item> priceComparator = (Comparator<Item>)new PriceComparator();
-        Item item1 = new Item(null, null, 31.0);
-        Item item2 = new Item(null, null, 34.0);
+        Item item1 = new Item(1L, "A", 31.0);
+        Item item2 = new Item(2L, "B", 34.0);
 
         // when
         Integer comparisonValue = priceComparator.compare(item1, item2);
