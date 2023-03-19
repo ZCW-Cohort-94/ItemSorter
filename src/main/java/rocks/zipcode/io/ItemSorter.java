708011deda735532ad1b84725f18c2fbb703ef58
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-19 17:44:48 -0400

    Lab finished and tested

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,31 @@
 package rocks.zipcode.io;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+
+    public Item[] getItems() {
+        return items;
+    }
+
+    public void setItems(Item[] items) {
+        this.items = items;
+    }
+
+    private Item[] items;
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Item[] itemsToSort = this.items;
+        Arrays.sort(itemsToSort, comparator);
+
+
+        return itemsToSort;
     }
 }
